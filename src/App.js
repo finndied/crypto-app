18d2d47ba8f9c667d5430bd2d409afa5314d7736
Ilya Morozov <finnlivefx1@mail.ru>
2023-04-10 18:37:03 +0700

    layout main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css'
 
-import { Switch, Route, Link } from 'react-router-dom'
+import { Route, Link, Routes } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
-import { Navbar } from './components'
+import {
+	Navbar,
+	Homepage,
+	Exchanges,
+	Cryptocurrencies,
+	CryptoDetails,
+	News
+} from './components'
 
 function App() {
 	return (
@@ -10,7 +17,19 @@ function App() {
 			<div className='navbar'>
 				<Navbar />
 			</div>
-			<div className='main'></div>
+			<div className='main'>
+				<Layout>
+					<div className='routes'>
+						<Routes>
+							<Route path='/' element={<Homepage />} />
+							<Route path='/exchanges' element={<Exchanges />} />
+							<Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+							<Route path='/crypto/:coinId' element={<CryptoDetails />} />
+							<Route path='/news' element={<News />} />
+						</Routes>
+					</div>
+				</Layout>
+			</div>
 			<div className='footer'></div>
 		</div>
 	)
