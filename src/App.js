dc74e449d267eec42b0b83acb3dff840c857826c
Ilya Morozov <finnlivefx1@mail.ru>
2023-04-10 17:30:09 +0700

    layout navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css'
 
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import { Navbar } from './components'
+
 function App() {
-	return <div className='App'>
-    <h1>CryptoApp</h1>
-  </div>
+	return (
+		<div className='App'>
+			<div className='navbar'>
+				<Navbar />
+			</div>
+			<div className='main'></div>
+			<div className='footer'></div>
+		</div>
+	)
 }
 
 export default App
